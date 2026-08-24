20c7110ec249ca69ae76b11eef621973ecf0d91e
AyselOzbakir <elifsu.aysel@gmail.com>
2026-08-24 19:44:17 +0200

    Initial commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
+    func playSound(note: String) {
+        if let url = Bundle.main.url(forResource: note, withExtension: "wav") {
+            player = try? AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } else {
+            print("Ses dosyası bulunamadı: \(note).wav")
+        }
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {let noteName = sender.currentTitle! 
+        playSound(note: noteName)
+        sender.alpha = 0.5
+        
+        DispatchQueue.main .asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1
+            
+        }
+    }
     
 
 }
